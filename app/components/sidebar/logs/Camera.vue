ba5d73ee63fdee74675962795657f3e50de43c50
Oli <squiglet07@gmail.com>
2023-10-14 14:19:02 +0100

    Commented code better

--- app/components/sidebar/logs/Camera.vue
+++ app/components/sidebar/logs/Camera.vue
@@ -9,6 +9,7 @@ import Vue from "vue";
 import { makeFullUrl } from "../../../store";
 
 var rnd = 0;
+//which IP should I use instead of localhost..?
 const cameraSocket = new WebSocket("ws://localhost:5001");//connects to websocket in shepherd websocks.py
 // Connection opened
 cameraSocket.addEventListener("open", (event) => {
