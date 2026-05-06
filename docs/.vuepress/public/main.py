3280ccc65b748f48f7fef8ac4c7e7c7f92638140
Nathan Gill <nathan.j.gill@outlook.com>
2026-05-06 16:50:28 +0100

    remove docs. docs are now built from the docs site instead for consistency

--- docs/.vuepress/public/main.py
+++
@@ -1,7 +0,0 @@
-import robot
-
-R = robot.Robot()
-
-print("Recovery complete!")
-print("Please turn off your robot and unplug the USB stick.")
-print("Then start the robot normally.")
--- docs/.vuepress/public/simple.py
+++
@@ -1,3 +0,0 @@
-import robot
-
-R = robot.Robot()
